dd06f09d0e57764ee3bd92b2a7a939aed436ecf6
Eric Toulson <Ebtoulson@gmail.com>
2014-07-14 10:01:39 -0400

    [ET][000000] fixed IE cookie passing by setting p3p headers

--- prototypes/portal_sample/app/controllers/application_controller.rb
+++ prototypes/portal_sample/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_p3p
 
   def require_login
     unless current_user
@@ -21,4 +22,8 @@ private
   end
 
   helper_method :current_user
+
+  def set_p3p
+    headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'
+  end
 end
